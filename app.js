3ed5b230d9354b1558c1f1ad2a571bb304974875
SanathRai <sanathrai03@gmail.com>
2026-05-12 22:54:08 +0530

    Express Server and it's different routes

--- app.js
+++ app.js
@@ -2,16 +2,31 @@ const express = require("express");
 
 const app = express();
 
-const userMiddleware = (req, res, next) => {
-  req.user = "Guest";
-  next();
-};
 
-app.get("/welcome", userMiddleware, (req, res) => {
-  res.send(`<h1>Welcome, ${req.user}!</h1>`);
-
+// GET Request to /orders
+app.get("/orders", (req, res) => {
+  res.send("Here is the list of all orders.");
 });
 
+
+// POST Request to /orders
+app.post("/orders", (req, res) => {
+  res.send("A new order has been created.");
+});
+
+
+// GET Request to /users
+app.get("/users", (req, res) => {
+  res.send("Here is the list of all users.");
+});
+
+
+// POST Request to /users
+app.post("/users", (req, res) => {
+  res.send("A new user has been added.");
+});
+
+
 app.listen(3000, () => {
-  console.log("Server running on port 3000");
+  console.log("Server is running on http://localhost:3000");
 });
