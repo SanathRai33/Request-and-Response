06532fe4d31562652b54ee3ff92dd54ec0b3cb11
SanathRai <sanathrai03@gmail.com>
2026-05-12 22:40:03 +0530

    Middleware: Updated app.js with user middleware

--- app.js
+++ app.js
@@ -2,12 +2,16 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("<h1>Welcome to Express Server</h1>");
+const userMiddleware = (req, res, next) => {
+  req.user = "Guest";
+  next();
+};
+
+app.get("/welcome", userMiddleware, (req, res) => {
+  res.send(`<h1>Welcome, ${req.user}!</h1>`);
+
 });
 
 app.listen(3000, () => {
-  console.log(
-    "Server is up and running on port 3000! Ready to handle requests."
-  );
+  console.log("Server running on port 3000");
 });
