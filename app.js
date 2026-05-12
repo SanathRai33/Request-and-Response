43111debead32a06b256ead50b845c5f570364b0
SanathRai <sanathrai03@gmail.com>
2026-05-12 23:31:12 +0530

    Dynamic Routes: params and query

--- app.js
+++ app.js
@@ -3,38 +3,20 @@ const express = require("express");
 const app = express();
 
 
-// GET /products
-app.get("/products", (req, res) => {
-  res.send("Here is the list of all products.");
-});
+app.get("/welcome/:username", (req, res) => {
 
+  const username = req.params.username;
 
-// POST /products
-app.post("/products", (req, res) => {
-  res.send("A new product has been added.");
-});
+  const age = req.query.age;
 
-
-// GET /categories
-app.get("/categories", (req, res) => {
-  res.send("Here is the list of all categories.");
-});
-
-
-// POST /categories
-app.post("/categories", (req, res) => {
-  res.send("A new category has been created.");
-});
-
-
-// WILDCARD ROUTE
-app.use("*", (req, res) => {
-  res.status(404).send("<h1>404 - Page Not Found</h1>");
+  res.send(
+    `<h1>Welcome ${username}, your age is ${age}</h1>`
+  );
 
 });
 
 
 // SERVER
-app.listen(4000, () => {
-  console.log("Server is running on http://localhost:4000");
+app.listen(3000, () => {
+  console.log("Server is running on http://localhost:3000");
 });
