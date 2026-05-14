2b10b5f483766fe73f49cf3c538d2a063047fd11
SanathRai <sanathrai03@gmail.com>
2026-05-14 22:48:03 +0530

    Implemented Express Router with separate route files

--- app.js
+++ app.js
@@ -1,41 +1,12 @@
 const express = require("express");
-
 const app = express();
+const shopRoutes = require("./routes/shop");
 
 
-// LOGGING MIDDLEWARE
-app.use((req, res, next) => {
-  console.log(`${req.method} request made to ${req.url}`);
-  next();
-});
+app.use("/shop", shopRoutes);
 
 
-// GET /products
-app.get("/products", (req, res) => {
-  res.send("Here is the list of all products.");
-});
-
-
-// POST /products
-app.post("/products", (req, res) => {
-  res.send("A new product has been added.");
-});
-
-
-// GET /categories
-app.get("/categories", (req, res) => {
-  res.send("Here is the list of all categories.");
-
-});
-
-
-// POST /categories
-app.post("/categories", (req, res) => {
-  res.send("A new category has been created.");
-
-});
-
-
+// SERVER
 app.listen(4000, () => {
   console.log("Server is running on http://localhost:4000");
 });
